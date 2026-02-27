b1fc5c62cc0438440eaba0c41e035672d180ee81
Brunopaivabjj <brunop1aiva@gmail.com>
2026-02-27 14:45:04 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+/* O conteúdo do JS que já passei na resposta anterior, incluindo: login, autenticação, modais, CRUD de serviços e profissionais, agendamentos e filtro por data.  
+A diferença: agora, ao carregar os agendamentos, mostramos o **nome do profissional** correto ao invés do ID. */
