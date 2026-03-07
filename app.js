01dcd40f1ce5af2a734c70d2e84db1bb2d419e93
Brunopaivabjj <brunop1aiva@gmail.com>
2026-03-07 20:19:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-document.getElementById("teste").addEventListener("click", function() {
-    alert("O Agendei está funcionando!");
-});
+const app = document.getElementById("app")
+
+document.getElementById("novoCliente").onclick = () => {
+    app.innerHTML = "<h2>Cadastrar cliente</h2>"
+}
+
+document.getElementById("novoAgendamento").onclick = () => {
+    app.innerHTML = "<h2>Novo agendamento</h2>"
+}
+
+document.getElementById("verAgenda").onclick = () => {
+    app.innerHTML = "<h2>Agenda</h2>"
+}
