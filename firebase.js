34edbbe8544313d586398c2abedb5a37395a4d20
Brunopaivabjj <brunop1aiva@gmail.com>
2026-03-07 20:32:56 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxyovmqjNYIzOYYDZZnduquiJQeK4UIgc",
+    authDomain: "agendei-d721e.firebaseapp.com",
+    projectId: "agendei-d721e",
+    storageBucket: "agendei-d721e.firebasestorage.app",
+    messagingSenderId: "525023801595",
+    appId: "1:525023801595:web:71a6d72e986e6e9e30005e",
+    measurementId: "G-YQP41N6MJ3"
+  };
+
+const app = initializeApp(firebaseConfig)
+
+export { app }
